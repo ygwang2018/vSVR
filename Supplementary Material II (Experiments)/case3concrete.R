ac6c16480ce7e1c433b9e5ac7a81457f2cabda09
You-Gan Wang <46190900+ygwang2018@users.noreply.github.com>
2021-02-01 22:19:54 +1000

    Update case3concrete.R

--- Supplementary Material II (Experiments)/case3concrete.R
+++ Supplementary Material II (Experiments)/case3concrete.R
@@ -21,7 +21,7 @@ DataSet<-cbind(Input,Output)
 
 SVs_results<-c()
 
-MSE_results<-c()
+RMSE_results<-c()
 
 MAE_results<-c()
 
@@ -35,7 +35,7 @@ for (i in c(1:100)){
   
 SVs<-c()
 
-MSE<-c()
+RMSE<-c()
 
 MAE<-c()
 
@@ -118,7 +118,7 @@ SVs[count]<-EpsSVR$tot.nSV/l
 
 EpsPreds<-predict(EpsSVR, Uniform_x_test)*(MaxY-MinY)+MinY
 
-MSE[count]<-sqrt(mean((EpsPreds-y_test)^2))
+RMSE[count]<-sqrt(mean((EpsPreds-y_test)^2))
 
 MAE[count]<-mean(abs(EpsPreds-y_test))
 
@@ -142,7 +142,7 @@ Prediction<-scale_prediction*Scale
 
 ## our optimal output
 
-MSE_Optimal<-sqrt(mean((Prediction-y_test)^2))
+RMSE_Optimal<-sqrt(mean((Prediction-y_test)^2))
 
 MAE_Optimal<-mean(abs(Prediction-y_test))
 
@@ -160,7 +160,7 @@ Optimal_Parameter<-rbind(Optimal_Parameter,cbind(Nu_Optimal,Eps_Optimal,Scale_Op
 
 SVs[count+1]<-SVs_Optimal
 
-MSE[count+1]<-MSE_Optimal
+RMSE[count+1]<-RMSE_Optimal
 
 MAE[count+1]<-MAE_Optimal
 
@@ -174,7 +174,7 @@ LinearFit=lm(y_train~.,data=SampleTrainData)
 
 LinearPred=predict(LinearFit,x_test)
 
-MSE[count+2]<-sqrt(mean((LinearPred-y_test)^2))
+RMSE[count+2]<-sqrt(mean((LinearPred-y_test)^2))
 
 MAE[count+2]<-mean(abs(LinearPred-y_test))
 
@@ -183,7 +183,7 @@ TreeFit=rpart(y_train~.,data=SampleTrainData)
 
 TreePred=predict(TreeFit,x_test)
 
-MSE[count+3]<-sqrt(mean((TreePred-y_test)^2))
+RMSE[count+3]<-sqrt(mean((TreePred-y_test)^2))
 
 MAE[count+3]<-mean(abs(TreePred-y_test))
 
@@ -193,14 +193,14 @@ NNFit=neuralnet(y_train~.,data = SampleTrainData)
 
 NNPred=predict(NNFit,x_test)
 
-MSE[count+4]<-sqrt(mean((NNPred-y_test)^2))
+RMSE[count+4]<-sqrt(mean((NNPred-y_test)^2))
 
 MAE[count+4]<-mean(abs(NNPred-y_test))
 
 
 SVs_results<-rbind(SVs_results,SVs)
 
-MSE_results<-rbind(MSE_results,MSE)
+RMSE_results<-rbind(RMSE_results,RMSE)
 
 MAE_results<-rbind(MAE_results,MAE)
 
@@ -208,7 +208,7 @@ Eps_results<-rbind(Eps_results,TotalEps)
 
 }
 
-write.csv(MSE_results,file="concrete_MSE.csv")
+write.csv(RMSE_results,file="concrete_RMSE.csv")
 write.csv(MAE_results,file="concrete_MAE.csv")
 
 write.csv(SVs_results,file="concrete_SVs.csv")
